2f7e2a36d3ad8ab6bc7ab91ff1066e6f738d345a
Phuc Dang <71363158+phucdh2@users.noreply.github.com>
2021-05-30 10:42:42 +0700

    Fix deploy-prod.sh script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -7,48 +7,44 @@ echo "SSH: Connecting to host for deployment of dev environment"
 
 # Some vars required in the script
 BASE_PATH="/var/www/shop-demo.dadventures.com"
-REPOS=( "user-activity-log" )
 DEPLOY_BRANCH="master"
 
 # Pull code from repository
-for repo in "${REPOS[@]}"
-do
-    echo "Pulling in the latest changes from repository ${repo}"
-    ( cd "${BASE_PATH}/${repo}" && git fetch --all --prune && git reset --hard origin/${DEPLOY_BRANCH} && git checkout origin/${DEPLOY_BRANCH} )
+echo "Pulling in the latest changes from repository"
+( cd "${BASE_PATH}" && git fetch --all --prune && git reset --hard origin/${DEPLOY_BRANCH} && git checkout origin/${DEPLOY_BRANCH} )
 
-    cd "${BASE_PATH}/${repo}/src"
+cd "${BASE_PATH}"
 
-    ls -al
+ls -al
 
-    echo "Copy .env"
-    php -r "file_exists('.env') || copy('.env.dev', '.env');"
+echo "Copy .env"
+php -r "file_exists('.env') || copy('.env.dev', '.env');"
 
-    echo "Generate key"
-    php artisan key:generate
+echo "Generate key"
+php artisan key:generate
 
-    echo "Directory Permissions"
-    chmod -R 777 storage bootstrap/cache
-    chmod a+rwx public -R
+echo "Directory Permissions"
+chmod -R 777 storage bootstrap/cache
+chmod a+rwx public -R
 
-    echo "Install dependencies"
-    #composer install
-    composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
-    #composer dump-autoload
+echo "Install dependencies"
+#composer install
+composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
+#composer dump-autoload
 
-    npm install
-    echo "Build assets"
-    npm run prod
+npm install
+echo "Build assets"
+npm run prod
 
-    echo "Migrate database"
-    php artisan migrate
+echo "Migrate database"
+php artisan migrate
 
-    echo "Clear cache"
-    php artisan config:clear
-    php artisan route:cache
-    php artisan cache:clear
-    php artisan view:clear
-    php artisan responsecache:clear
+echo "Clear cache"
+php artisan config:clear
+php artisan route:cache
+php artisan cache:clear
+php artisan view:clear
+php artisan responsecache:clear
 
-done
 
 echo "Deploy to prod environment completed"
