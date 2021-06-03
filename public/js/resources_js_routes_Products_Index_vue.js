aa7c0bdc9843fdc0539e0bd106d4746849647604
Andrew Huynh <tanphuqn@gmail.com>
2021-06-03 17:08:19 +0700

    Update gen user table

--- public/js/resources_js_routes_Products_Index_vue.js
+++ public/js/resources_js_routes_Products_Index_vue.js
@@ -70,12 +70,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-//
-//
-//
-//
-//
-//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   methods: {
     formatCurrency: function formatCurrency(amount) {
